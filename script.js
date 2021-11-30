206dfb933e12491211331c70451ad802309b1377
AkhileshPonnappa <65938519+AkhileshPonnappa@users.noreply.github.com>
2021-11-30 16:29:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let colors = ['red','blue', 'green','yellow','black','grey','purple','turqoise','pink'];
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+
+    var randomColor = colors[Math.floor(Math.random() * colors.length)]
+
+    let container = document.getElementById('container');
+
+    container.style.background = randomColor;
+})
